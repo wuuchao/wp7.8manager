fd51002391452c86fba94bba570b065d4ec39592
unknown <ichao@tju.edu.cn>
2013-05-17 23:42:12 +0800

    20131221.zip

---
+++ connection/AdressIp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 背单词对战.connection
+{
+    class AdressIp
+    {
+        private string adressUri = "wuuchao.cloudapp.net";
+        private string wpServerUri = "wuuchao.cloudapp.net/wpServer";
+
+        public string getAdressUri()
+        {
+            return adressUri;
+        }
+
+        public string getWpServerUri()
+        {
+            return wpServerUri;
+        }
+    }
+}
