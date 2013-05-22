59a12d9b542b15e593d5f3cd164f717a01098f27
zhangyifann <390290573@qq.com>
2013-05-22 23:14:44 +0800

    wuchao

---
+++ setLimit.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using 背单词对战.connection;
+
+namespace 背单词对战.view
+{
+    public partial class setLimit : PhoneApplicationPage
+    {
+        private string conn = null;
+        public setLimit()
+        {
+            InitializeComponent();
+            connector con = new connector();
+            AdressIp add = new AdressIp();
+            conn = con.Connect("http://wuuchao.cloudapp.net/wpServer/", 80);
+            con.Send("111");
+            conn = con.Receive();
+            t.Text = conn;
+        }
+
+    }
+}
